dec0fca5293c79cbe7a1decec1062aae48144698
Tammy <tammycwong@gmail.com>
2021-06-17 22:33:04 -0400

    reseed

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,9 +3,18 @@ class Event < ApplicationRecord
     has_many :rsvps
     has_many :users, through: :rsvps
 
-    def all_attending_users 
-        self.rsvps.map do |rsvp|
-            rsvp.user
+    # def all_attending_users 
+    #     self.rsvps.map do |rsvp|
+    #         rsvp.user
+    #     end
+    # end
+
+    def all_attending_users
+        self.users.map do |user|
+            user.name
         end
     end
+
+    # def hosted_by
+    # end
 end
